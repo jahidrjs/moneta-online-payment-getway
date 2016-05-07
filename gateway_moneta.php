ed96ee6f47d76e2beebd810c9bfc7f60f1dce0e0
jahid al mamun <rjs.jahid11@gmail.com>
2016-05-07 11:10:59 +0600

    Update gateway_moneta.php

--- gateway_moneta.php
+++ gateway_moneta.php
@@ -5,10 +5,7 @@ class Gateway_moneta extends CI_Controller {
     public function __Construct(){
         parent::__construct();	
 
-/*        if(!$this->session->userdata('is_logged'))
-        {
-               redirect('', 'refresh');
-        }*/
+
     }
 
     public function index()
@@ -25,48 +22,12 @@ class Gateway_moneta extends CI_Controller {
 		*/
 		$responseToMerchantUrl = base_url_tr('gateway_moneta/notify');
 		
-		if($from==3) //"3" means payment is coming from "payment_due"
-		{
-			$SCAP_ID = $this->session->userdata('SCAP_ID');
-			
-			//keep the next line for future purpose
-		    //$amount = $this->session->userdata('due_amount');
-
-			$details = $this->model_common->select_single_row("scap_payments_due","SCAP_ID",$SCAP_ID);
-			$amount = $details["SCAP_AMOUNT"];
-			$merchantOrderId = $SCAP_ID;
-			$method_and_id = "moneta,".$SCAP_ID.",".$from.",".$amount;
-		}
-		
-		
-		if($from==2) //"2" means payment is coming from "renewal"
-		{
-			$SUBS_ID = $this->session->userdata('SUBS_ID');
-			
-			//$details = $this->model_common->select_single_row("subs_subscriptions","SUBS_ID",$SUBS_ID);
-			$amount = $this->session->userdata('SUBS_AMOUNT_PAYED');;
-			$merchantOrderId = $SUBS_ID;
-			$method_and_id = "moneta,".$SUBS_ID.",".$from.",".$amount;
-		}
-		
-		
-		if($from==1) //"1" means payment is coming from "subscription"
-		{
-			$SUBS_ID = $this->session->userdata('SUBS_ID');
-			
-			//$details = $this->model_common->select_single_row("subs_subscriptions","SUBS_ID",$SUBS_ID);
-			$amount = $this->session->userdata('SUBS_AMOUNT_PAYED');
-			$merchantOrderId = $SUBS_ID;
-			$method_and_id = "moneta,".$SUBS_ID.",".$from.",".$amount;
-		}
-		
-		
 		    //test id: 97702600, password: smF14001
-			//production id: 97702600, password: Pippo2406
-		
+		$merchantOrderId = '987956';
+		$method_and_id = 'some value';
 		$parameters = array(
 			'id' => '97702600', // Terminal Id
-			'password' => 'Pippo2406',
+			'password' => 'smF14001',
 			'operationType' => 'initialize',
 			'amount' => $amount,
 			'currencyCode' => '978',
@@ -156,178 +117,20 @@ class Gateway_moneta extends CI_Controller {
 		$this->model_common->update('subs_subscriptions',$data,"SUBS_ID",$id);
 		
 				//--- start: send email
-		$SUBS_ID = $id;
-		$STUD_ID = $this->model_common->select_single_field("STUD_ID","subs_subscriptions","SUBS_ID",$SUBS_ID);
-		$STUD_EMAIL = $this->model_common->select_single_field("STUD_MAIL_MAIL_1","stud_students","STUD_ID",$STUD_ID);
-		
-		//***************** just for checking
-/*		$info = array();
-		$info["method_and_id"] = $method_and_id;
-		$info["sub_id"] = $SUBS_ID;
-		$info["result"] = $result;
-		$info["transaction_id"] = $paymentID;
-		$info["from"] = $from;
-
-		$log['call_time'] = date("Y-m-d h:i:s");
-        $log['call_ip'] = $this->input->ip_address();
-        $log['post'] = $info;
-        $file = fopen("./moneta_log.txt","a+");
-        if($file){
-            fwrite($file, json_encode($log)."\r\n\r\n");
-        }
-        fclose($file);
-*/
-		//********************
-		
-		$sub = "Scuola di Musica di Fiesole"." | ".date("d-m-Y");
-		$msg = "Caro Studente, <br/>
-                        il tuo pagamento per l'iscrizione è stato ricevuto.<br/><br/>
-
-                        Cordiali Saluti,<br/>
-                        Segreteria Studenti<br/>
-                        Scuola di Musica di Fiesole <br/><br/>
-
-                        <hr/>
-
-                        Dear Student,<br/>
-                        Your payment for subscription was successful.<br/><br/>
-
-                        Best Regards<br/>
-                        Students' Office<br/>
-                        Scuola di Musica di Fiesole. <br/>
-                        ";
-		
-		$this->custom->send_email($STUD_EMAIL,$email_from="",$sub,$msg); //to, from, subject, msg
+		 // you send send email with this information hereeeeeeeee.
 				//--- end: send email
 		//$ResultURL = base_url_tr("gateway_moneta/result/".$result."/1/".$id);
 		}
-		elseif(($result!="APPROVED" or $result!="CAPTURED") and ($from=="1"))
-		{
-			$SUBS_ID = $id;
-			$this->model_common->delete("subs_subscriptions","SUBS_ID",$SUBS_ID);
-		}
-
-
 		
-		// for payments of "renewal"
-		if(($method=="moneta") and (($result=="APPROVED") or ($result=="CAPTURED")) and ($from=="2"))
-		{
-		$data = array();
-		$data["SUBS_TRANSACTION_ID"] = $paymentID;
-		$data["SUBS_AMOUNT_PAYED"] = $SUBS_AMOUNT_PAYED;
-		$this->model_common->update('subs_subscriptions',$data,"SUBS_ID",$id);
-		
-				//--- start: send email
-		$SUBS_ID = $id;
-		$STUD_ID = $this->model_common->select_single_field("STUD_ID","subs_subscriptions","SUBS_ID",$SUBS_ID);
-		$STUD_EMAIL = $this->model_common->select_single_field("STUD_MAIL_MAIL_1","stud_students","STUD_ID",$STUD_ID);
-		
-		$sub = "Scuola di Musica di Fiesole"." | ".date("d-m-Y");
-		$msg = "Caro Studente, <br/>
-                        il tuo pagamento per l'iscrizione è stato ricevuto.<br/><br/>
-
-                        Cordiali Saluti,<br/>
-                        Segreteria Studenti<br/>
-                        Scuola di Musica di Fiesole <br/><br/>
-
-                        <hr/>
-
-                        Dear Student,<br/>
-                        Your payment for subscription was successful.<br/><br/>
-
-                        Best Regards<br/>
-                        Students' Office<br/>
-                        Scuola di Musica di Fiesole. <br/>
-                        ";
-		
-		$this->custom->send_email($STUD_EMAIL,$email_from="",$sub,$msg); //to, from, subject, msg
-				//--- end: send email
-		//$ResultURL = base_url_tr("gateway_moneta/result/".$result."/2/".$id);
-		}
-		elseif(($result!="APPROVED" or $result!="CAPTURED") and ($from=="2"))
-		{
-			$SUBS_ID = $id;
-			$this->model_common->delete("subs_subscriptions","SUBS_ID",$SUBS_ID);
-		}
-		
-		
-		// for payments of "payment due"
-		if(($method=="moneta") and (($result=="APPROVED") or ($result=="CAPTURED")) and ($from=="3"))
-		{
-		$data = array();
-		$data["SCAP_TRANSACTION_ID"] = $paymentID;
-		$data["PAYT_DAI_ID"] = $this->model_common->select_single_field("PAYT_DAI_ID","payt_payment_types","PAYT_ID","1");
-		$data["SCAP_AMOUNT_PAYED"] = $this->model_common->select_single_field("SCAP_AMOUNT","scap_payments_due","SCAP_ID",$id);
-		$data["CC_UPD_DATA"] = date("Y-m-d H:i:s");
-                $this->model_common->update('scap_payments_due',$data,"SCAP_ID",$id);
-		
-						//--- start: send email
-		$SCAP_ID = $id;
-		$STUD_ID = $this->model_common->select_single_field("STUD_ID","scap_payments_due","SCAP_ID",$SCAP_ID);
-		$STUD_EMAIL = $this->model_common->select_single_field("STUD_MAIL_MAIL_1","stud_students","STUD_ID",$STUD_ID);
-		
-		$sub = "Scuola di Musica di Fiesole"." | ".date("d-m-Y");
-		$msg = "Caro Studente, <br/>
-                        il tuo pagamento per l'iscrizione è stato ricevuto.<br/><br/>
-
-                        Cordiali Saluti,<br/>
-                        Segreteria Studenti<br/>
-                        Scuola di Musica di Fiesole <br/><br/>
-
-                        <hr/>
-
-                        Dear Student,<br/>
-                        Your payment for subscription was successful.<br/><br/>
-
-                        Best Regards<br/>
-                        Students' Office<br/>
-                        Scuola di Musica di Fiesole. <br/>
-                        ";
-		
-		$this->custom->send_email($STUD_EMAIL,$email_from="",$sub,$msg); //to, from, subject, msg
-				//--- end: send email
-		
-		//$ResultURL = base_url_tr("gateway_moneta/result/".$result."/3/".$id);
-		}
 		
 		$ResultURL = base_url_tr("gateway_moneta/result/" . $result);
-		
+		// then echo return url for moneta 
 		echo $ResultURL; // Send to MonetaWeb server 
     }
 	
-	
+	// last return url function 
 	public function result($result="")
 	{
- 		
-/*	  if($from == 3)
-	  {
-						//--- start: send email
-		$SCAP_ID = $id;
-		$STUD_ID = $this->model_common->select_single_field("STUD_ID","scap_payments_due","SCAP_ID",$SCAP_ID);
-		$STUD_EMAIL = $this->model_common->select_single_field("STUD_MAIL_MAIL_1","stud_students","STUD_ID",$STUD_ID);
-		
-		$sub = "Scuola di Musica di Fiesole"." | ".date("d-m-Y");
-		$msg = "Caro Studente, <br/>
-                        il tuo pagamento per l'iscrizione è stato ricevuto.<br/><br/>
-
-                        Cordiali Saluti,<br/>
-                        Segreteria Studenti<br/>
-                        Scuola di Musica di Fiesole <br/><br/>
-
-                        <hr/>
-
-                        Dear Student,<br/>
-                        Your payment for subscription was successful.<br/><br/>
-
-                        Best Regards<br/>
-                        Students' Office<br/>
-                        Scuola di Musica di Fiesole. <br/>
-                        ";
-		
-		$this->custom->send_email($STUD_EMAIL,$email_from="",$sub,$msg); //to, from, subject, msg
-				//--- end: send email
-	  }*/
-		
 	  $data["payment_status"] = $result;
 	  $data['header']['title'] = "Payment Status";
 	  $data['body']['page'] = "payment_made_status";
